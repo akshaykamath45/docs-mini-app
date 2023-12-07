fc1a18bd95e1f4c801831c1bbe8fc0c28d36612f
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-07 17:36:55 +0530

    refactor :  background component

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div>
+      <div className="absolute w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl top-[5%]">
+        Documents.
+      </div>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[256px]  font-semibold leading-none tracking-tighter text-zinc-900">
+        Docs.
+      </h1>
+    </div>
+  );
+};
+
+export default Background;
