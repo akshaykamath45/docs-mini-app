36169b535a4e071d96921186632a9bcb18ec7b34
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-07 17:34:50 +0530

    style : designing background

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <div className='absolute w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl top-[5%]'>Documents.</div>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[256px]  font-semibold leading-none tracking-tighter text-zinc-900">Docs.</h1>
     </div>
   );
 };
